12082843e5f2b2ef86195b9a744664d6ee2c8c23
ololina <alinaudodova@gmail.com>
2022-10-09 12:13:24 +0200

    final project tests

---
+++ src/test/java/constants/Credentials.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Credentials {
+    public static final String USERNAME = System.getProperty("username");
+    public static final String PASSWORD = System.getProperty("password");
+}
